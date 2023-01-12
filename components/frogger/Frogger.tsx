2ddcb13ae5bdde724e98bd3315a5ca636a1674b2
Daan Weustenraad <weustenraad@gmail.com>
2023-01-12 09:37:56 +0100

    add cool arrow navigation to frogger

--- components/frogger/Frogger.tsx
+++ components/frogger/Frogger.tsx
@@ -2,10 +2,10 @@ import { observer } from "mobx-react";
 import { useNavigation } from "../../stores/NavigationStore";
 import { TMenuItemID } from "../../typings/navigation";
 import { Frog } from "./Frog";
-import styles from "../../styles/components/Frogger.module.scss";
 import { frogger, FroggerContext } from "../../stores/FroggerStore";
 import { Target } from "./Target";
 import { Score } from "./Score";
+import { ArrowHelp } from "./ArrowHelp";
 
 export const Frogger: React.FC = observer(() => {
 	const { activeMenuItem } = useNavigation();
@@ -18,7 +18,7 @@ export const Frogger: React.FC = observer(() => {
 		<FroggerContext.Provider value={frogger}>
 			<Frog />
 			<Target />
-			<img className={styles.arrows} src="/frogger/arrow-icons.jpg" alt="arrows" />
+			<ArrowHelp />
 			<Score />
 		</FroggerContext.Provider>
 	);
